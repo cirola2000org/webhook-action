a2573aa9b9abddb76b055ee56b212cbaa688052e
Ciro Baron Neto <cirobaroneto@gmail.com>
2020-06-26 13:09:51 -0400

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+DATA_JSON="\"repository\":\"$GITHUB_REPOSITORY\",\"ref\":\"$GITHUB_REF\",\"commit\":\"$GITHUB_SHA\",\"trigger\":\"$GITHUB_EVENT_NAME\",\"workflow\":\"$GITHUB_WORKFLOW\""
+
+if [ -n "$data" ]; then
+    COMPACT_JSON=$(echo -n "$data" | jq -c '')
+    WEBHOOK_DATA="{$DATA_JSON,\"data\":$COMPACT_JSON}"
+    # WEBHOOK_DATA=$(jq -c . $GITHUB_EVENT_PATH)
+else
+    WEBHOOK_DATA="{$DATA_JSON}"
+fi
+
+WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
+
+WEBHOOK_ENDPOINT=$webhook_url
+WEGHOOK_GUID="896668ab-8924-4196-9653-8aca50720d46"
+
+curl -X POST \ 
+    -H "content-type: application/json" \
+    -H "User-Agent: User-Agent: GitHub-Hookshot/760256b" \
+    -H "x-hub-signature: sha1=$WEBHOOK_SIGNATURE" \
+    -H "x-gitHub-delivery: $WEGHOOK_GUID" \
+    -H "x-github-event: $GITHUB_EVENT_NAME" \
+    --data "$WEBHOOK_DATA" $WEBHOOK_ENDPOINT
