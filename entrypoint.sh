77fc8261ade41bda88edab5620929fe63153d449
Ciro Baron Neto <cirobaroneto@gmail.com>
2020-06-26 13:37:50 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 set -eu
 
-DATA_JSON="\"repository\":\"$GITHUB_REPOSITORY\",\"ref\":\"$GITHUB_REF\",\"commit\":\"$GITHUB_SHA\",\"trigger\":\"$GITHUB_EVENT_NAME\",\"workflow\":\"$GITHUB_WORKFLOW\""
-
-WEBHOOK_DATA="{$DATA_JSON}"
-
+WEBHOOK_DATA=="\"repository\":\"$GITHUB_REPOSITORY\",\"ref\":\"$GITHUB_REF\",\"commit\":\"$GITHUB_SHA\",\"trigger\":\"$GITHUB_EVENT_NAME\",\"workflow\":\"$GITHUB_WORKFLOW\""
 WEBHOOK_SIGNATURE=$(echo -n "$WEBHOOK_DATA" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
-
 WEBHOOK_ENDPOINT=$webhook_url
-WEGHOOK_GUID="896668ab-8924-4196-9653-8aca50720d46"
+WEBHOOK_GUID="896668ab-8924-4196-9653-8aca50720d46"
 
 curl -X POST \ 
     -H "content-type: application/json" \
     -H "User-Agent: User-Agent: GitHub-Hookshot/760256b" \
     -H "x-hub-signature: sha1=$WEBHOOK_SIGNATURE" \
-    -H "x-gitHub-delivery: $WEGHOOK_GUID" \
+    -H "x-gitHub-delivery: $WEBHOOK_GUID" \
     -H "x-github-event: $GITHUB_EVENT_NAME" \
     --data "$WEBHOOK_DATA" $WEBHOOK_ENDPOINT
